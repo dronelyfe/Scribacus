94531b521f95930ec6757ac592d4310796aba019
dronelyfe <greg.m.wilson@hotmail.ca>
2019-01-18 21:01:18 -0500

    further project structure changes, addition of electron and p5 for frontend

---
+++ src/scribalizer/lizer.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron')
+
+// const _ = require('electron-reload')(__dirname + '/public')
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // center: true,
+        // frame: false,
+        // resizable: false,
+        // alwaysOnTop: true,
+        useContentSize: true
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'render', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.webContents.openDevTools("undock")
+
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
---
+++ src/scribalizer/render/draw.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  // console.log(`${windowWidth}, ${windowHeight}`)
+  noStroke()
+  background(255)
+}
+
+function draw() {
+  background(255)
+}
+
+function mousePressed() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
